cbb56c62b4d70560789edc5b6210bfcdad4780f6
mriyuh <mriyuh.yt@gmail.com>
2025-05-21 20:04:55 +0100

    Update fetch-latest-video.js

--- site/scripts/fetch-latest-video.js
+++ site/scripts/fetch-latest-video.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch'); // v2 recommended for CommonJS
 const API_KEY = process.env.YOUTUBE_API_KEY; 
 const CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;
 
-const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=1&type=video`;
+const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=1`;
 
 fetch(url)
   .then(res => res.json())
